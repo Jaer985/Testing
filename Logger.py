f58fc8d43bf372a5a39284f5f604a76d58c39623
Jaer985 <jaer1909@gmail.com>
2022-08-29 23:08:44 -0400

    Log into txt completed

--- Logger.py
+++ Logger.py
@@ -1,4 +1,9 @@
-#Logger
+# This is a logger
+# every line you add
+# gets added to a file
+# in this case is called log.txt
+# and to exit you need to write exit
+# 
 
 datawr = input("Data log: ")
 while True:
